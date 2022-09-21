c526dcb7f662831da5c2aac316977d1ef8a919fb
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-21 15:54:54 +0100

    Adjust and cleanup workflows

---
+++ Testing/CICDTests/FakeTests.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="FakeTest.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using Xunit;
+
+namespace CICDTests;
+
+public class FakeTests
+{
+    [Fact]
+    public void FakeTest()
+    {
+        Assert.True(true);
+    }
+}
