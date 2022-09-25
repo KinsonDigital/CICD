628740a5a48ebb02a33dee359a61e3d708c47a7a
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-25 23:30:03 +0100

    [new] - Create new service to return the execution context of the build system

---
+++ CICD/Services/ExecutionContextService.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="ExecutionContextService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace CICDSystem.Services;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+public class ExecutionContextService : IExecutionContextService
+{
+    /// <inheritdoc/>
+    public bool IsLocalBuild => Nuke.Common.NukeBuild.IsLocalBuild;
+
+    /// <inheritdoc/>
+    public bool IsServerBuild => Nuke.Common.NukeBuild.IsServerBuild;
+}
