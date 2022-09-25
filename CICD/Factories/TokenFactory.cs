6a7e40ab93637e3c679bf22dc274b4b8b5dd1133
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-25 23:29:26 +0100

    [new] - Create new service to load local or server tokens

---
+++ CICD/Factories/TokenFactory.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="TokenFactory.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using CICDSystem.Guards;
+using CICDSystem.Services;
+using Nuke.Common.CI.GitHubActions;
+
+// ReSharper disable InconsistentNaming
+namespace CICDSystem.Factories;
+
+/// <inheritdoc/>
+public class TokenFactory : ITokenFactory
+{
+    private readonly ILoadSecretsService secretsService;
+    private readonly IExecutionContextService executionContextService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TokenFactory"/> class.
+    /// </summary>
+    /// <param name="secretsService">Loads secrets locally.</param>
+    /// <param name="executionContextService">Determines the execution context.</param>
+    public TokenFactory(
+        ILoadSecretsService secretsService,
+        IExecutionContextService executionContextService)
+    {
+        EnsureThat.ParamIsNotNull(secretsService, nameof(secretsService));
+        EnsureThat.ParamIsNotNull(executionContextService, nameof(executionContextService));
+
+        this.secretsService = secretsService;
+        this.executionContextService = executionContextService;
+    }
+
+    /// <inheritdoc/>
+    public string GetToken() =>
+        this.executionContextService.IsServerBuild
+            ? GitHubActions.Instance.Token // NOTE: This branch is not to be tested
+            : this.secretsService.LoadSecret("GithubAPIToken");
+}
---
+++ Testing/CICDTests/Factories/TokenFactoryTests.cs
@@ -0,0 +1,80 @@
+﻿// <copyright file="TokenFactoryTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using CICDSystem.Factories;
+using CICDSystem.Services;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CICDSystemTests.Factories;
+
+/// <summary>
+/// Tests the <see cref="TokenFactory"/> class.
+/// </summary>
+public class TokenFactoryTests
+{
+    private readonly Mock<ILoadSecretsService> mockSecretsService;
+    private readonly Mock<IExecutionContextService> mockExecutionService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TokenFactoryTests"/> class.
+    /// </summary>
+    public TokenFactoryTests()
+    {
+        this.mockSecretsService = new Mock<ILoadSecretsService>();
+        this.mockExecutionService = new Mock<IExecutionContextService>();
+    }
+
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WithNullSecretsServiceParam_ThrowsException()
+    {
+        // Arrange & Act
+        var act = () =>
+        {
+            _ = new TokenFactory(null, this.mockExecutionService.Object);
+        };
+
+        // Assert
+        act.Should()
+            .Throw<ArgumentNullException>()
+            .WithMessage("The parameter must not be null. (Parameter 'secretsService')");
+    }
+
+    [Fact]
+    public void Ctor_WithNullExecutionContextServiceParam_ThrowsException()
+    {
+        // Arrange & Act
+        var act = () =>
+        {
+            _ = new TokenFactory(this.mockSecretsService.Object, null);
+        };
+
+        // Assert
+        act.Should()
+            .Throw<ArgumentNullException>()
+            .WithMessage("The parameter must not be null. (Parameter 'executionContextService')");
+    }
+    #endregion
+
+    #region Method Tests
+
+    [Fact]
+    public void GetToken_WhenInvokedInLocalExecutionContext_ReturnCorrectValue()
+    {
+        // Arrange
+        this.mockSecretsService.Setup(m => m.LoadSecret("GithubAPIToken"))
+            .Returns("test-token");
+
+        var factory = new TokenFactory(this.mockSecretsService.Object, this.mockExecutionService.Object);
+
+        // Act
+        var actual = factory.GetToken();
+
+        // Assert
+        actual.Should().Be("test-token");
+    }
+    #endregion
+}
