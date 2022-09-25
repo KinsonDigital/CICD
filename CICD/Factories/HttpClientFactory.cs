f389fb53ccbcb260682897f5380b4527bc545d41
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-25 23:29:49 +0100

    [new] - Create factory to create HTTP clients

---
+++ CICD/Factories/HttpClientFactory.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="HttpClientFactory.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using CICDSystem.Guards;
+using Octokit;
+using Octokit.Internal;
+
+// ReSharper disable InconsistentNaming
+namespace CICDSystem.Factories;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+public sealed class HttpClientFactory : IHttpClientFactory
+{
+    private static IGitHubClient? client;
+    private bool isDisposed;
+
+    /// <inheritdoc/>
+    public IGitHubClient CreateGitHubClient(string productName, string token)
+    {
+        if (client is not null)
+        {
+            return client;
+        }
+
+        EnsureThat.StringParamIsNotNullOrEmpty(productName, nameof(productName));
+        EnsureThat.StringParamIsNotNullOrEmpty(token, nameof(token));
+
+        var productHeaderValue = new ProductHeaderValue(productName);
+        var credStore = new InMemoryCredentialStore(new Credentials(token));
+
+        client = new GitHubClient(productHeaderValue, credStore);
+
+        return client;
+    }
+}
