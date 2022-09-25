fa9c11fd6aeaccf93cf57d026181f1b9a1537c60
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-25 19:03:52 +0100

    Create service to get the current execution assembly

---
+++ CICD/Services/CurrentDirService.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="CurrentDirService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Abstractions;
+using System.Reflection;
+
+namespace CICDSystem.Services;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+public class CurrentDirService : ICurrentDirService
+{
+    private readonly IPath path;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CurrentDirService"/> class.
+    /// </summary>
+    /// <param name="path">Manages paths.</param>
+    public CurrentDirService(IPath path) => this.path = path;
+
+    /// <inheritdoc/>
+    public string GetCurrentDirectory() => $"{this.path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}";
+}
