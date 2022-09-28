cdc0319739a8a3c88f4e50dc587d58b4b3b781fc
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-28 23:21:53 +0100

    Create scripts to automatically build and install the CICD dotnet tool locally for testing

---
+++ Testing/DotNetToolTesting/CreateToolPackage.ps1
@@ -0,0 +1,32 @@
+Clear-Host;
+
+$toolProjName = "CICD";
+$projectFile = "$toolProjName.csproj";
+$toolTestingDirPath = $PSScriptRoot;
+$nugetPackageOutputDirPath = "$toolTestingDirPath/packages";
+
+if (Test-Path -Path $nugetPackageOutputDirPath) {
+    # Delete all nuget packages
+    Remove-Item -Path "$nugetPackageOutputDirPath" -Recurse -Confirm:$false;
+}
+
+[string]$repoRootDirPath = $PSScriptRoot.Replace("\", "/");
+
+# Split the path into sections
+[System.Collections.ArrayList]$sections = $repoRootDirPath.Split("/");
+
+# Remove the last 2 directories
+$sections.RemoveRange($sections.Count - 2, 2);
+
+# Back up 2 directories to the repo root
+$repoRootDirPath = "";
+$sections | ForEach-Object -Process { $repoRootDirPath = "$repoRootDirPath/$_" };
+$repoRootDirPath = $repoRootDirPath.TrimStart("/");
+
+$toolProjectPath = "$repoRootDirPath/$toolProjName/$projectFile";
+
+# Build the project
+dotnet build "$toolProjectPath" -c "Debug";
+
+# Create the nuget package
+dotnet pack $toolProjectPath -c "Debug" -o $nugetPackageOutputDirPath;;
