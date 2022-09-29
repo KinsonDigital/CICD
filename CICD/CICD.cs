92e3360562299ba7c0e97907428eebc948e05d18
Calvin Wilkinson <kinsondigital@gmail.com>
2022-09-29 13:51:24 +0100

    Add descriptions to all of the build parameters

--- CICD/CICD.cs
+++ CICD/CICD.cs
@@ -9,7 +9,6 @@ using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Octokit;
-using NukeParameter = Nuke.Common.ParameterAttribute;
 
 namespace CICDSystem;
 
@@ -39,50 +38,50 @@ public partial class CICD : NukeBuild
 
     private IGitRepoService repo => App.Container.GetInstance<IGitRepoService>();
 
-    [NukeParameter]
+    [Parameter("The output directory of where the template workflows should be generated.")]
     private string? WorkflowTemplateOutput { get; set; }
 
-    [NukeParameter]
-    private static bool SkipTwitterAnnouncement { get; set; }
+    [Parameter("If true, will skip the twitter announcement of a release.")]
+    private bool SkipTwitterAnnouncement { get; set; }
 
-    [NukeParameter]
+    [Parameter("The owner of the GitHub repo.  This can also be the GitHub organization that owns the repository.")]
     private string RepoOwner { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The name of the GitHub repository.")]
     private string RepoName { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The name of the C# project.")]
     private string ProjectName { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The unique number/id of the GItHub pull request.  Used for pull request status checks when running locally.")]
     private int PullRequestNumber { get; set; }
 
-    [NukeParameter]
+    [Parameter($"The name of the preview release notes directory name.  This will be located in the '{nameof(ReleaseNotesBaseDirPath)}'.")]
     private string PreviewReleaseNotesDirName { get; set; } = "PreviewReleases";
 
-    [NukeParameter]
+    [Parameter($"The name of the production release notes directory name.  This will be located in the '{nameof(ReleaseNotesBaseDirPath)}'.")]
     private string ProductionReleaseNotesDirName { get; set; } = "ProductionReleases";
 
-    [NukeParameter]
+    [Parameter("The directory path of the location of the preview and production release notes.")]
     private AbsolutePath ReleaseNotesBaseDirPath { get; set; } = RootDirectory / "Documentation" / "ReleaseNotes";
 
-    [NukeParameter]
+    [Parameter("The API key for releasing NuGet packages to nuget.org.")]
     [Secret]
     private string NugetOrgApiKey { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The Twitter consumer API key.  Essentially the Twitter username.")]
     [Secret]
     private string TwitterConsumerApiKey { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The Twitter consumer API secret.  Essentially the Twitter password.")]
     [Secret]
     private string TwitterConsumerApiSecret { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The Twitter access token.")]
     [Secret]
     private string TwitterAccessToken { get; set; } = string.Empty;
 
-    [NukeParameter]
+    [Parameter("The Twitter access token secret.")]
     [Secret]
     private string TwitterAccessTokenSecret { get; set; } = string.Empty;
 
