331d2bdad9c6b4221ed547367f8711dd59a4c4dd
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-04 18:48:50 +0100

    Create core Reactable API with unit tests

---
+++ Testing/CICDTests/Reactables/Core/ReactableTests.cs
@@ -0,0 +1,104 @@
+﻿// <copyright file="ReactableTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using CICDSystem.Reactables.Core;
+using CICDSystemTests.Fakes;
+using CICDSystemTests.Helpers;
+using Moq;
+using Xunit;
+
+namespace CICDSystemTests.Reactables.Core;
+
+/// <summary>
+/// Tests the <see cref="Reactable{TData}"/> class.
+/// </summary>
+public class ReactableTests
+{
+    #region Method Tests
+    [Fact]
+    public void Subscribe_WithNullReactorParam_ThrowsException()
+    {
+        // Arrange
+        var reactable = CreateReactable<bool>();
+
+        // Act & Assert
+        AssertExtensions.ThrowsWithMessage<ArgumentNullException>(() =>
+        {
+            reactable.Subscribe(null);
+        }, "The parameter must not be null. (Parameter 'reactor')");
+    }
+
+    [Fact]
+    public void Subscribe_WhenAddingNewReactor_ReactorAddedToReactable()
+    {
+        // Arrange
+        var reactable = CreateReactable<bool>();
+
+        // Act
+        reactable.Subscribe(new Reactor<bool>());
+
+        // Assert
+        Assert.Single(reactable.Reactors);
+    }
+
+    [Fact]
+    public void Subscribe_WhenAddingNewReactor_ReturnsUnsubscriber()
+    {
+        // Arrange
+        var reactable = CreateReactable<bool>();
+        var reactor = new Reactor<bool>();
+
+        // Act
+        var actual = (ReactorUnsubscriber<bool>)reactable.Subscribe(reactor);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<ReactorUnsubscriber<bool>>(actual);
+        Assert.Same(reactor, actual.Reactor);
+    }
+
+    [Fact]
+    public void EndNotifications_WhenInvoked_CompletesAllReactors()
+    {
+        // Arrange
+        var reactable = CreateReactable<bool>();
+        var mockReactorA = new Mock<IReactor<bool>>();
+        var mockReactorB = new Mock<IReactor<bool>>();
+
+        reactable.Subscribe(mockReactorA.Object);
+        reactable.Subscribe(mockReactorB.Object);
+
+        // Act
+        reactable.EndNotifications();
+        reactable.EndNotifications();
+
+        // Assert
+        mockReactorA.Verify(m => m.OnCompleted(), Times.Once);
+    }
+
+    [Fact]
+    public void Dispose_WhenInvokedWithReactors_RemovesReactors()
+    {
+        // Arrange
+        var reactable = CreateReactable<bool>();
+
+        reactable.Subscribe(new Reactor<bool>());
+
+        // Act
+        reactable.Dispose();
+        reactable.Dispose();
+
+        // Assert
+        Assert.Empty(reactable.Reactors);
+    }
+    #endregion
+
+    /// <summary>
+    /// Creates a new instance of the abstract <see cref="Reactable{TData}"/> for the purpose of testing.
+    /// </summary>
+    /// <typeparam name="T">The type of data that the reactable will deal with.</typeparam>
+    /// <returns>The instance to test.</returns>
+    private static ReactableFake<T> CreateReactable<T>()
+        => new ();
+}
