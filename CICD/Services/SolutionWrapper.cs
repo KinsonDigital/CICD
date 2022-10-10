1446dc7b6090ad58191fb7b8a18993fb1ec49771
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-10 16:45:53 +0100

    Create thin wrapper around the NUKE solution object

---
+++ CICD/Services/SolutionWrapper.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="SolutionWrapper.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+// ReSharper disable InconsistentNaming
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using CICDSystem.Reactables.Core;
+using Nuke.Common.ProjectModel;
+
+namespace CICDSystem.Services;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+internal sealed class SolutionWrapper : ISolutionWrapper
+{
+    private readonly IDisposable? unsubscriber;
+    private Solution? solution;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SolutionWrapper"/> class.
+    /// </summary>
+    /// <param name="solutionReactable">Provides push notifications about the build solution.</param>
+    public SolutionWrapper(IReactable<Solution> solutionReactable) =>
+        this.unsubscriber = solutionReactable.Subscribe(new Reactor<Solution>(
+            onNext: solutionData => this.solution = solutionData,
+            () => this.unsubscriber?.Dispose()));
+
+    /// <inheritdoc/>
+    public string Directory => this.solution?.Directory?.ToString().Replace('\\', '/').TrimEnd('/') ?? string.Empty;
+
+    /// <inheritdoc/>
+    public IReadOnlyCollection<Project> AllProjects => this.solution?.AllProjects ?? new ReadOnlyCollection<Project>(Array.Empty<Project>());
+
+    /// <inheritdoc/>
+    public Project? GetProject(string nameOrFullPath) => this.solution?.GetProject(nameOrFullPath);
+
+    /// <inheritdoc/>
+    public IEnumerable<Project> GetProjects(string wildcardPattern) => this.solution?.GetProjects(wildcardPattern) ?? Array.Empty<Project>();
+}
