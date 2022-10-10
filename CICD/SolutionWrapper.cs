856e03991744656a08fd9d83cddfa4f6321ce0d2
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-10 17:19:04 +0100

    Create unit tests to test the SolutionWrapper ctor

--- CICD/SolutionWrapper.cs
+++ CICD/SolutionWrapper.cs
@@ -7,13 +7,13 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
+using CICDSystem.Guards;
 using CICDSystem.Reactables.Core;
 using Nuke.Common.ProjectModel;
 
 namespace CICDSystem;
 
 /// <inheritdoc/>
-[ExcludeFromCodeCoverage]
 internal sealed class SolutionWrapper : ISolutionWrapper
 {
     private readonly IDisposable? unsubscriber;
@@ -23,20 +23,28 @@ internal sealed class SolutionWrapper : ISolutionWrapper
     /// Initializes a new instance of the <see cref="SolutionWrapper"/> class.
     /// </summary>
     /// <param name="solutionReactable">Provides push notifications about the build solution.</param>
-    public SolutionWrapper(IReactable<Solution> solutionReactable) =>
+    public SolutionWrapper(IReactable<Solution> solutionReactable)
+    {
+        EnsureThat.ParamIsNotNull(solutionReactable, nameof(solutionReactable));
+
         this.unsubscriber = solutionReactable.Subscribe(new Reactor<Solution>(
             onNext: solutionData => this.solution = solutionData,
             () => this.unsubscriber?.Dispose()));
+    }
 
     /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
     public string Directory => this.solution?.Directory?.ToString().Replace('\\', '/').TrimEnd('/') ?? string.Empty;
 
     /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
     public IReadOnlyCollection<Project> AllProjects => this.solution?.AllProjects ?? new ReadOnlyCollection<Project>(Array.Empty<Project>());
 
     /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
     public Project? GetProject(string nameOrFullPath) => this.solution?.GetProject(nameOrFullPath);
 
     /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
     public IEnumerable<Project> GetProjects(string wildcardPattern) => this.solution?.GetProjects(wildcardPattern) ?? Array.Empty<Project>();
 }
