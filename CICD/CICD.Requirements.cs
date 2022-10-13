97e2511a4f9f38758afe48f8d8148b95f7d9c9de
Calvin Wilkinson <kinsondigital@gmail.com>
2022-10-13 16:20:32 +0100

    Make simple logging adjustment

--- CICD/CICD.Requirements.cs
+++ CICD/CICD.Requirements.cs
@@ -1274,9 +1274,9 @@ public partial class CICD // Requirements
 
         if (errors.Count <= 0)
         {
-            var logMsg = $"The production release '{prodVersion}' does not have any previous preview releases.";
+            var logMsg = $"{ConsoleTab}The production release '{prodVersion}' does not have any previous preview releases.";
             logMsg += $"{Environment.NewLine}{ConsoleTab}Release notes check for preview release items complete.";
-            Log.Information(logMsg);
+            Console.WriteLine(logMsg);
             return true;
         }
 
