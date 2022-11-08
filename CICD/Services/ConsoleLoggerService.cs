183da9ce3be7db97e66ca5d1023265e58700fded
Calvin Wilkinson <kinsondigital@gmail.com>
2022-11-08 09:38:55 +0000

    Create a logger service

---
+++ CICD/Services/ConsoleLoggerService.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="ConsoleLoggerService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using CICDSystem.Services.Interfaces;
+using Serilog;
+
+namespace CICDSystem.Services;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+internal sealed class ConsoleLoggerService : IConsoleLoggerService
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConsoleLoggerService"/> class.
+    /// </summary>
+    public ConsoleLoggerService() =>
+        Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
+
+    /// <inheritdoc/>
+    public ILogger Logger { get; }
+}
