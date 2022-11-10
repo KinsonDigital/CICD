51587f4632b5a6f441277e2c8d5d1463e87f8900
Calvin Wilkinson <kinsondigital@gmail.com>
2022-11-10 16:33:01 +0000

    [feat] - Create service for project related functionality

---
+++ CICD/Services/ProjectService.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="ProjectService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+// ReSharper disable InconsistentNaming
+using System;
+using System.Diagnostics.CodeAnalysis;
+using CICDSystem.Guards;
+using CICDSystem.Reactables.Core;
+using CICDSystem.Services.Interfaces;
+
+namespace CICDSystem.Services;
+
+/// <inheritdoc/>
+internal sealed class ProjectService : IProjectService
+{
+    private readonly ISolutionWrapper solutionWrapper;
+    private readonly IDisposable unsubscriber;
+    private string projectName = string.Empty;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProjectService"/> class.
+    /// </summary>
+    /// <param name="solutionWrapper">
+    ///     Wraps the <see cref="Nuke.Common"/>.
+    ///     <see cref="Nuke.Common.ProjectModel"/>.
+    ///     <see cref="Nuke.Common.ProjectModel.Solution"/> functionality.
+    /// </param>
+    /// <param name="repoInfoReactable">Provides push notifications about repository information.</param>
+    public ProjectService(ISolutionWrapper solutionWrapper, IReactable<(string, string)> repoInfoReactable)
+    {
+        EnsureThat.ParamIsNotNull(solutionWrapper, nameof(solutionWrapper));
+        EnsureThat.ParamIsNotNull(repoInfoReactable, nameof(repoInfoReactable));
+
+        this.solutionWrapper = solutionWrapper;
+
+        this.unsubscriber = repoInfoReactable.Subscribe(new Reactor<(string repoOwner, string repoName)>(
+            onNext: repoInfoData =>
+            {
+                this.projectName = repoInfoData.repoName;
+            }, () => this.unsubscriber?.Dispose()));
+    }
+
+    /// <inheritdoc/>
+    [ExcludeFromCodeCoverage]
+    public string GetVersion()
+    {
+        var project = this.solutionWrapper.GetProject(this.projectName);
+
+        if (project is null)
+        {
+            throw new Exception($"The project '{this.projectName}' could not be found.");
+        }
+
+        var version = project.GetVersion();
+
+        version = version.StartsWith("v")
+            ? version
+            : $"v{version}";
+
+        return version;
+    }
+}
