31fc57d0c806e99845a8c167429eab01e9994f91
Calvin Wilkinson <kinsondigital@gmail.com>
2022-11-10 16:32:17 +0000

    [feat] - Create service to send release tweets

---
+++ CICD/Services/Interfaces/ITweetService.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ITweetService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace CICDSystem.Services.Interfaces;
+
+/// <summary>
+/// Provides tweet related functionality.
+/// </summary>
+internal interface ITweetService
+{
+    /// <summary>
+    /// Sends a release tweet.
+    /// </summary>
+    void SendReleaseTweet();
+}
