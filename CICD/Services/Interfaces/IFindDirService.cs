34e422a61dac0a92438767833c4aa2e7db267181
Calvin Wilkinson <kinsondigital@gmail.com>
2022-11-14 17:29:43 +0000

    feat: Create service for directory searching

---
+++ CICD/Services/Interfaces/IFindDirService.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="IFindDirService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace CICDSystem.Services.Interfaces;
+
+/// <summary>
+/// Provides directory search functionality.
+/// </summary>
+public interface IFindDirService
+{
+    /// <summary>
+    /// Walks up one directory at a time starting at the given <paramref name="startPath"/>
+    /// until a directory with a name that matches the given <paramref name="dirNameToFind"/>.
+    /// </summary>
+    /// <param name="startPath">The directory path to start the search from.</param>
+    /// <param name="dirNameToFind">The name to the directory to find.</param>
+    /// <returns>The directory path that contains the <paramref name="dirNameToFind"/>.</returns>
+    string FindDescendentDir(string? startPath, string dirNameToFind);
+}
