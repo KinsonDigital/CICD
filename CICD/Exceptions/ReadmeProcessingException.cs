010c32b4cbdfc1f8ecd749ec3d23af74fdfcdf36
Calvin Wilkinson <kinsondigital@gmail.com>
2022-11-15 00:24:13 +0000

    feat: Create custom exception for readme processing errors

---
+++ CICD/Exceptions/ReadmeProcessingException.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="ReadmeProcessingException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace CICDSystem.Exceptions;
+
+/// <summary>
+/// Thrown when there is an issue with pre-processing a README file.
+/// </summary>
+internal sealed class ReadmeProcessingException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReadmeProcessingException"/> class.
+    /// </summary>
+    public ReadmeProcessingException()
+        : base("There was an issue with pre-processing the README file.")
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReadmeProcessingException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public ReadmeProcessingException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ReadmeProcessingException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">
+    ///     The <see cref="Exception"/> instance that caused the current exception.
+    /// </param>
+    public ReadmeProcessingException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
