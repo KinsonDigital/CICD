984b1575d3acf70f99027ba24bdf70dc2422436a
Calvin Wilkinson <kinsondigital@gmail.com>
2022-12-14 17:55:29 +0000

    chore: create models to receive data from NuGet API

---
+++ CICD/Models/NugetPackageResponse.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="NugetPackageResponse.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+namespace CICDSystem.Models;
+
+using System;
+
+/// <summary>
+/// Holds NuGet package response data.
+/// </summary>
+public class NugetPackageResponse
+{
+    /// <summary>
+    /// Gets the list of NuGet packages of a NuGet API request.
+    /// </summary>
+    public NugetPackageModel[] Data { get; init; } = Array.Empty<NugetPackageModel>();
+}
